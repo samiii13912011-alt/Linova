0ae8b8b826d3a945675e91b78d21decf0b5346c1
samiii13912011-alt <samiii13912011@gmail.com>
2026-08-19 14:06:03 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-        alert("😎 به Linova خوش اومدی!");
-    }, 800);
+
+    const btn = document.getElementById("themeBtn");
+
+    btn.addEventListener("click", () => {
+
+        document.body.classList.toggle("light-mode");
+
+        if (document.body.classList.contains("light-mode")) {
+            btn.textContent = "🌙 حالت تیره";
+        } else {
+            btn.textContent = "☀️ حالت روشن";
+        }
+
+    });
+
 });
