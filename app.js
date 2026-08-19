0459565bd72ee6224e00874a7302eaafb895a005
samiii13912011-alt <samiii13912011@gmail.com>
2026-08-19 21:23:52 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById("themeBtn");
 
+// دکمه حالت روشن/تیره
 btn.onclick = function () {
     document.body.classList.toggle("light-mode");
 
@@ -9,3 +10,25 @@ btn.onclick = function () {
         btn.textContent = "☀️ حالت روشن";
     }
 };
+
+// تابع شمارنده
+function animateCounter(id, target) {
+    const element = document.getElementById(id);
+
+    if (!element) return;
+
+    let value = 0;
+
+    const timer = setInterval(() => {
+        value++;
+        element.textContent = value;
+
+        if (value >= target) {
+            clearInterval(timer);
+        }
+    }, 20);
+}
+
+// اجرای شمارنده‌ها
+animateCounter("users", 25);
+animateCounter("visits", 350);
