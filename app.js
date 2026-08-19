c30292023bbda21a3872ddfa51fcec0fd87998c8
samiii13912011-alt <samiii13912011@gmail.com>
2026-08-19 15:43:05 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
+alert("مرحله 1");
 
-    const themeBtn = document.getElementById("themeBtn");
-
-    themeBtn.addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-
-        if (document.body.classList.contains("light-mode")) {
-            themeBtn.textContent = "🌙 حالت تیره";
-        } else {
-            themeBtn.textContent = "☀️ حالت روشن";
-        }
-    });
-
-    animateCounter("users", 25);
-    animateCounter("visits", 350);
-
-});
-
-
-function animateCounter(id, target) {
-
-    const element = document.getElementById(id);
-
-    if (!element) {
-        alert("عنصر " + id + " پیدا نشد!");
-        return;
-    }
-
-    let value = 0;
-
-    const timer = setInterval(() => {
-
-        value += 1;
-        element.textContent = value;
-
-        if (value >= target) {
-            clearInterval(timer);
-        }
-
-    }, 20);
-}
+document.getElementById("themeBtn").onclick = function () {
+    alert("دکمه کار می‌کند 😎");
+};
